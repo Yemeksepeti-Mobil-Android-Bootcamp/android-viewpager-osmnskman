71ac0ae709f50ab453e09bcee212641350b01c4b
osmnskman <osmnskman@gmail.com>
2021-07-15 22:32:30 +0300

    first commit

--- app/src/main/java/com/osmansekman/andorid_viewpager_osmnskman/fragment/onboardingAllFragment.kt
+++ app/src/main/java/com/osmansekman/andorid_viewpager_osmnskman/fragment/onboardingAllFragment.kt
@@ -5,11 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
-import com.osmansekman.andorid_viewpager_osmnskman.R
+import androidx.viewpager2.widget.ViewPager2
 import com.osmansekman.andorid_viewpager_osmnskman.databinding.FragmentOnboardingBinding
 import com.osmansekman.andorid_viewpager_osmnskman.utils.ViewPagerAdapter
-import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 class onboardingAllFragment: Fragment() {
     private lateinit var binding: FragmentOnboardingBinding
@@ -18,16 +16,35 @@ class onboardingAllFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentOnboardingBinding.inflate(layoutInflater,container,false)
+        binding = FragmentOnboardingBinding.inflate(layoutInflater, container, false)
         val view = binding.root
         initViewPager()
         return view
     }
+
     private fun initViewPager() {
         val adapter = ViewPagerAdapter(requireActivity())
         binding.viewPager.adapter = adapter
         binding.dotsIndicator.setViewPager2(binding.viewPager)
 
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+               if (position == 2) {
+                   binding.skipButton.setOnClickListener{
+                       binding.viewPager.currentItem = 0
+                   }
+                   }
+                else
+               {
+                   binding.skipButton.setOnClickListener{
+                       binding.viewPager.currentItem = binding.viewPager.currentItem +1
+                   }
+               }
+            }
+
+        })
     }
+
 
 }
